631f32c7ab382c8be7a7e0be7932a0d03d997167
8bit <ejthbit@gmail.com>
2024-04-18 10:35:35 +0200

    - Fixes ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import js from '@eslint/js'
-import reactRecommended from 'eslint-plugin-react/configs/recommended.js'
+import reactRecommendedPlugin from 'eslint-plugin-react'
+import hooksPlugin from 'eslint-plugin-react-hooks'
 
 export default [
     js.configs.recommended,
-    reactRecommended,
     {
-
+        plugins: { react: reactRecommendedPlugin, 'react-hooks': hooksPlugin },
         rules: {
             'no-undef': 'warn',
             'no-unused-vars': 'warn',
@@ -14,6 +14,11 @@ export default [
             'react/no-direct-mutation-state': 'off',
             'react/prop-types': 'off',
             'react/require-render-return': 'off',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
         },
     },
 ]
